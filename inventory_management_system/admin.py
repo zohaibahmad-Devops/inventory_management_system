12218173ddfc32bc0351b1c6bb06512251d10e12
zohaibahmad-Devops <zohaibahmas.bs.cs@gmail.com>
2021-01-22 01:51:33 -0800

    2nd commit

--- inventory_management_system/admin.py
+++ inventory_management_system/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-
+from .forms import StockCreateForm
 from .models import Stock
 
 
-admin.site.register(Stock)
+class StockCreateAdmin(admin.ModelAdmin):
+    list_display = ['category', 'item_name', 'quantity', 'issue_by']
+    form = StockCreateForm
+    list_filter = ['category']
+    search_fields = ['category', 'item_name']
+
+
+admin.site.register(Stock, StockCreateAdmin)
