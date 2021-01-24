d9d22d7b37855c381ac92f11a14c7124a69a28bd
zohaibahmad-Devops <zohaibahmas.bs.cs@gmail.com>
2021-01-24 13:57:08 +0500

    5th

--- inventory_management_system/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Stock
-
-
-class StockCreateForm(forms.ModelForm):
-    class Meta:
-        model = Stock
-        fields = ['category', 'item_name', 'quantity']
--- inventory_management_system/models.py
+++ inventory_management_system/models.py
@@ -1,18 +1,14 @@
 from django.db import models
 
 
-class Stock(models.Model):
+class Add_product(models.Model):
     category = models.CharField(max_length=50, blank=True, null=True)
     item_name = models.CharField(max_length=50, blank=True, null=True)
     quantity = models.IntegerField(default='0', blank=True, null=True)
     receive_quantity = models.IntegerField(default=0, blank=True, null=True)
     receive_by = models.CharField(max_length=50, blank=True, null=True)
-    issue_quantity = models.IntegerField(default=0, blank=True, null=True)
-    issue_by = models.CharField(max_length=50, blank=True, null=True)
-    issue_to = models.CharField(max_length=50, blank=True, null=True)
-    phone_number = models.CharField(max_length=50, blank=True, null=True)
-    created_by = models.CharField(max_length=50, blank=True, null=True)
-    reorder_level = models.IntegerField(default=0, blank=True, null=True)
+    price_in = models.IntegerField(default=0, blank=True, null=True)
+    price_out = models.IntegerField(default=0, blank=True, null=True)
     last_update = models.DateTimeField(auto_now_add=False, auto_now=True)
     export_to_CSV = models.BooleanField(default=False)
 
